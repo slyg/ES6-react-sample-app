5e4fb55083616095e5c6dc22c6e1863d33d4cee8
slyg <syl.faucherand@gmail.com>
2015-05-02 22:52:23 +0200

    Add CSS build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,36 @@
-var gulp = require('gulp'),
-  browserify = require('browserify'),
-  babelify = require('babelify'),
-  source = require('vinyl-source-stream');
+var gulp      = require('gulp'),
+  browserify  = require('browserify'),
+  babelify    = require('babelify'),
+  source      = require('vinyl-source-stream'),
+  sass        = require('gulp-sass'),
+  bower       = require('main-bower-files'),
+  concat      = require('gulp-concat'),
+  es          = require('event-stream');
 
-gulp.task('build', function () {
-  browserify({
+const DEST_FOLDER = 'dist';
+const MAIN_CSS = 'style.css';
+const MAIN_JS = 'app.js';
+
+gulp.task('js', function () {
+
+  return browserify({
     entries: './src/js/app.js',
     extensions: ['.js'],
     debug: true
   })
-  .transform(babelify)
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('dist'));
+    .transform(babelify)
+    .bundle()
+    .pipe(source(MAIN_JS))
+    .pipe(gulp.dest(DEST_FOLDER));
 });
 
-gulp.task('default', ['build']);
+gulp.task('css', function(){
+  var vendor = gulp.src(bower()),
+      app    = gulp.src('./src/css/*.scss').pipe(sass());
+
+  return es.concat(vendor, app)
+    .pipe(concat(MAIN_CSS))
+    .pipe(gulp.dest(DEST_FOLDER));
+});
+
+gulp.task('default', ['js', 'css']);
